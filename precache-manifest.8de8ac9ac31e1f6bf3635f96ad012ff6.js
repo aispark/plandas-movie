e4c13ab2cf670dcac6a1b26121a51be81ceab6d4
aispark <aispark@paran.com>
2019-10-03 13:52:01 +0900

    Updates

---
+++ precache-manifest.8de8ac9ac31e1f6bf3635f96ad012ff6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73fc96cc86f2c84ce4a92cff95983517",
+    "url": "/plandas-movie/index.html"
+  },
+  {
+    "revision": "cf7238dcb6fdcc3670cc",
+    "url": "/plandas-movie/static/css/main.479b8a3a.chunk.css"
+  },
+  {
+    "revision": "f6af147729fa6965ae8f",
+    "url": "/plandas-movie/static/js/2.360b590f.chunk.js"
+  },
+  {
+    "revision": "cf7238dcb6fdcc3670cc",
+    "url": "/plandas-movie/static/js/main.6b1c4eee.chunk.js"
+  },
+  {
+    "revision": "b3d76b4a23ae4a731a37",
+    "url": "/plandas-movie/static/js/runtime-main.c4c72d74.js"
+  }
+]);
